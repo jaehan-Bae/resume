3d97ba7dbfa3f5e3ccc102bca2f57f1787e7c848
jaehan-Bae <yanggo31@gmail.com>
2023-06-02 10:16:01 +0900

    코드 하나 빠짐

--- js/custom.js
+++ js/custom.js
@@ -10,31 +10,31 @@ $(function () {
 
   
   //Wheel Scroll Chage
-    // window.addEventListener("wheel", function (e) {
-    //   e.preventDefault();
-    // }, { passive: false });
+    window.addEventListener("wheel", function (e) {
+      e.preventDefault();
+    }, { passive: false });
 
-    // var $html = $("html");
-    // var page = 1;
-    // var lastPage = $("section").length;
+    var $html = $("html");
+    var page = 1;
+    var lastPage = $("section").length;
 
-    // var timeScrollAnimation = 50;
-    // $html.animate({ scrollTop: 0 }, 10);
+    var timeScrollAnimation = 50;
+    $html.animate({ scrollTop: 0 }, 10);
 
-    // $(window).on("wheel", function (e) {
-    //   if ($html.is(":animated")) return;
+    $(window).on("wheel", function (e) {
+      if ($html.is(":animated")) return;
 
-    //   if (e.originalEvent.deltaY > 0) { 
-    //     if (page == lastPage) return;
-    //     page++;
-    //   } else if (e.originalEvent.deltaY < 0) { 
-    //     if (page == 1) return;
-    //   }
+      if (e.originalEvent.deltaY > 0) { 
+        if (page == lastPage) return;
+        page++;
+      } else if (e.originalEvent.deltaY < 0) { 
+        if (page == 1) return;
+        page--;
+      }
 
-    //   var posTop = (page - 1) * ($(window).height());
-    //   $html.animate({ scrollTop: posTop }, timeScrollAnimation);
-
-    // });
+      var posTop = (page - 1) * ($(window).height());
+      $html.animate({ scrollTop: posTop }, timeScrollAnimation);
+    });
 
     $('.portfolio-item').click(function() {
       $(this).addClass('active');
